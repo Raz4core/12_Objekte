a89f619698fe99aafdfbcb9a6a983edc1be933e0
Raz4core <mail@test.de>
2022-01-25 11:14:14 +0100

    Nested arrays and loops

--- script_01.js
+++ script_01.js
@@ -1,6 +1,25 @@
 
 /***** Erinnerung: Arrays *******/
 
+let arr;
+// arr = ["Geh","dich","retten!"];
+// output(arr);
+// output(arr[0]);
+
+
+// nested arrays
+arr = [["Geh","dich","retten!"],["Bevor","du","fällst!"]];
+output(arr[0][2]);
+output(arr[1][2]);
+
+// nested loops
+ for (let i = 0; i < 2; i++) {
+         for (let j = 0; j < 3; j++){
+        output(arr[i][j]);
+         }
+        
+ }
+
 
 /***** Objekte 1 Daten/Funktionen *******/
 
