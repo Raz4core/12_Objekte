437511a0b368a9ef6b66464853999c3d37eff2b4
Raz4core <mail@test.de>
2022-01-25 12:57:55 +0100

    Objekte 2 - Hierarchie

--- script_01.js
+++ script_01.js
@@ -24,31 +24,51 @@ let arr;
 
 /***** Objekte 1 Daten/Funktionen *******/
 
-let person = 
-        {
-                firstName: "Thilo",
-                familyName:"Schönemeyer",
-                salery:[120000,160000],
-                permission: true,
-                sayHello: 
-                        function() {
-                            return "Hello, ich bin " + this.firstName;    
-                        }
+// let person = 
+//         {
+//                 firstName: "Thilo",
+//                 familyName:"Schönemeyer",
+//                 salery:[120000,160000],
+//                 permission: true,
+//                 sayHello: 
+//                         function() {
+//                             return "Hello, ich bin " + this.firstName;    
+//                         }
      
-        };      
+//         };      
 
 
-// output(person);
- person.firstName = "Thilo";
-// output(person.firstName);
-// output(person.familyName);
+// // output(person);
+//  person.firstName = "Thilo";
+// // output(person.firstName);
+// // output(person.familyName);
 
-const txt = "Ich bin " + person.firstName + " " +
-                person.familyName + " und verdiene " +
-                person.salery[1] + " p.a"
-output(txt);
+// const txt = "Ich bin " + person.firstName + " " +
+//                 person.familyName + " und verdiene " +
+//                 person.salery[1] + " p.a"
+// output(txt);
 
-output(person.sayHello());
+// output(person.sayHello());
+
+/****Objekte 2 - Hierarchie ****/
+
+//Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein licht // K.Bush
+// Der Baikalsee ist mit 1642m der tiefste see auf der Erde.
+// deep - deeper - deepest
+
+const baikal = {
+
+        value:"10m",
+        deep:{
+                deeper:{
+                        deepest:"Das Licht - auf 1642m!"
+                }
+        }
+};
+
+output(baikal);
+output(baikal.value);
+output(baikal.deep.deeper.deepest);
 
 /** Ausgabe */
 function output(outputData) {
