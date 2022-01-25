3c0a7e089514a5c2e657f23bedc60b0e89a8c397
Raz4core <mail@test.de>
2022-01-25 11:53:31 +0100

    Objekte: Daten/Funktionen

--- script_01.js
+++ script_01.js
@@ -7,24 +7,34 @@ let arr;
 // output(arr[0]);
 
 
-// nested arrays
-arr = [["Geh","dich","retten!"],["Bevor","du","fällst!"]];
-output(arr[0][2]);
-output(arr[1][2]);
+// // nested arrays
+// arr = [["Geh","dich","retten!"],["Bevor","du","fällst!"]];
+// output(arr[0][2]);
+// output(arr[1][2]);
+// output("--------");
 
-// nested loops
- for (let i = 0; i < 2; i++) {
-         for (let j = 0; j < 3; j++){
-        output(arr[i][j]);
-         }
+// // nested loops
+//  for (let i = 0; i < 2; i++) {
+//          for (let j = 0; j < arr[0].length; j++){
+//         output(arr[i][j]);
+//          }
         
- }
+//  }
 
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+let person = 
+        {
+                firstName: "Thilo",
+                familyName:"Schönemeyer"
+
+        };      
 
 
+output(person);
+output(person.firstName);
+output(person.familyName);
 
 /** Ausgabe */
 function output(outputData) {
